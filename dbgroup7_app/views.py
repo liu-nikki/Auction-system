285d7e96c3844347cc46d020f46d103307ed5d2e
Swag Master <swagmaster@Swags-MacBook-Pro.local>
2024-04-09 13:23:19 -0700

    added phone

--- dbgroup7_app/views.py
+++ dbgroup7_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+
 # Create your views here.
 def testmysql(req):
     return render(req, 'home.html')
+
+
+# def phone(req):
+#     return render(req, 'phone.html')
+
+def phone(request):
+    phones = Phone.objects.all()
+    return render(request, 'phone.html', {'phones': phones})
