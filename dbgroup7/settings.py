27746624259e9fd9309547204b085438c271d2f4
Swag Master <swagmaster@Swags-MacBook-Pro.local>
2024-04-17 13:23:11 -0700

    added images

--- dbgroup7/settings.py
+++ dbgroup7/settings.py
@@ -148,8 +148,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = 'static'
-STATIC_URL = "static/"
+# STATIC_ROOT = 'static'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
